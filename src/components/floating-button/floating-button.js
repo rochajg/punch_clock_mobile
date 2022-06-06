a0340ac05222ee06bcade9b27c0821059ff38110
Gabriel Belgamo <19699724+belgamo@users.noreply.github.com>
2022-06-06 19:54:08 -0300

    refactor: add submenu to fab (#25)

--- src/components/floating-button/floating-button.js
+++ src/components/floating-button/floating-button.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import FistIcon from "@assets/icons/fist.png";
+import { Icon } from "./floating-button.styles";
+import { FloatingAction as RNFab } from "react-native-floating-action";
+import { useTheme } from "styled-components";
+import { moderateScale, verticalScale } from "react-native-size-matters";
+import { FontAwesome5 } from "@expo/vector-icons";
+import theme from "../../styles/theme";
 
-import FistIcon from '@assets/icons/fist.png';
+const routeActions = {
+  multiple_punches: "CalendarScreen",
+  single_punch: "NewPunchScreen",
+};
 
-import { Container, Icon } from './floating-button.styles';
+const actions = [
+  {
+    text: "Múltiplos punches",
+    icon: (
+      <FontAwesome5
+        name="calendar-alt"
+        size={20}
+        color={theme.colors.primary}
+      />
+    ),
+    name: "multiple_punches",
+    position: 2,
+    color: theme.colors.white,
+  },
+  {
+    text: "Novo punch",
+    icon: (
+      <FontAwesome5
+        name="calendar-day"
+        size={20}
+        color={theme.colors.primary}
+      />
+    ),
+    name: "single_punch",
+    position: 1,
+    color: theme.colors.white,
+  },
+];
 
 const FloatingButton = () => {
   const navigation = useNavigation();
 
   return (
-    <Container onPress={() => navigation.navigate('CalendarScreen')}>
-      <Icon source={FistIcon}></Icon>
-    </Container>
+    <RNFab
+      actions={actions}
+      onPressItem={(name) => {
+        navigation.navigate(routeActions[name]);
+      }}
+      color={theme.colors.primary}
+      floatingIcon={<Icon source={FistIcon}></Icon>}
+      buttonSize={moderateScale(70)}
+      distanceToEdge={{
+        horizontal: moderateScale(18),
+        vertical: verticalScale(18),
+      }}
+    />
   );
 };
 
--- src/components/floating-button/floating-button.styles.js
+++ src/components/floating-button/floating-button.styles.js
@@ -1,30 +1,5 @@
-import styled from 'styled-components/native';
-
-import { moderateScale } from 'react-native-size-matters';
-
-export const Container = styled.TouchableOpacity`
-  position: absolute;
-
-  width: ${moderateScale(70)}px;
-  height: ${moderateScale(70)}px;
-
-  border-radius: ${moderateScale(35)}px;
-
-  bottom: ${moderateScale(20)}px;
-  right: ${moderateScale(16)}px;
-
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  shadow-color: #000;
-  shadow-offset: 0px ${moderateScale(5)}px;
-  shadow-opacity: 0.34;
-  shadow-radius: ${moderateScale(6.27)}px;
-
-  elevation: 10;
-
-  justify-content: center;
-  align-items: center;
-`;
+import styled from "styled-components/native";
+import { moderateScale } from "react-native-size-matters";
 
 export const Icon = styled.Image`
   width: ${moderateScale(36)}px;
